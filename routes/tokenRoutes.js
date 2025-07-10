a328a2cf1ac727922c5d4b8b8b95afa3871a72ab
Vikramop <veervikramsingh132@gmail.com>
2025-07-10 11:10:38 +0530

    clean up

--- routes/tokenRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controller/tokenController');
-
-router.post('/refresh', controller.refresh);
-router.post('/logout', controller.logout);
-// router.post('/token/refresh', controller.refreshToken);
-
-module.exports = router;
